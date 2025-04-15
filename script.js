fe6fdea08d75483b3de1b5d805f9bc9ec734615d
Mrprofuture <mrprofuture@gmail.com>
2025-04-16 04:58:03 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function trackDelivery() {
-  const code = document.getElementById('deliveryCode').value;
+  const code = document.getElementById('AYUSH').value;
   const output = document.getElementById('statusOutput');
 
-  if (!code) {
+  if (!AYUSH) {
     output.innerHTML = "Please enter a delivery code.";
     return;
   }
 
   // For demo: fake response
-  output.innerHTML = `Tracking info for code <strong>${code}</strong>:<br>Status: GHAR KE BAHAR BANI 🛵`;
+  output.innerHTML = `Tracking info for code <strong>${AYUSH}</strong>:<br>Status: GHAR KE BAHAR BANI 🛵`;
 }
